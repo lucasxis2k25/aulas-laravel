9b6c3ae1a32bd8f3c34c7c2b7b7197193974bcc4
LUCAS CAMPOS RIBEIRO <lucas.cribeiro11@senacsp.edu.br>
2025-06-09 11:42:39 -0300

    aula-1

--- routes/web.php
+++ routes/web.php
@@ -11,34 +11,3 @@ Route::get('/sobrenos', function (){
 
 });
 
-Route::get('/home', function (){
-   return 'essa é a pagina home';
-
-});
-
-
-Route::get('/curses', function (){
-   return 'essa pagina contém os cursos';
-
-});
-
-
-Route::get('/log-in', function (){
-   return 'essa é a pagina de login';
-
-});
-
-Route::get('/cadastro', function (){
-   return 'essa é a pagina de cadastro';
-
-});
-
-Route::get('/recu-senha', function (){
-   return 'essa é a pagina de recuperação de senha';
-
-});
-
-Route::get('/meus-cursos', function (){
-   return 'essa é a pagina de cursos';
-
-});
